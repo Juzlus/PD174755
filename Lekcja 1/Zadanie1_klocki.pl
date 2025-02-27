dc9939e7a099dc903f07eee75c3ae82aff896cdb
Juzlus <juzlus.biznes@gmail.com>
2025-02-27 13:01:12 +0100

    Add files via upload

---
+++ Lekcja 1/Zadanie1_klocki.pl
@@ -0,0 +1,25 @@
+% PROGRAM: klicki_1
+% Baza wiedzy o ukdładzie klocków
+% Definiowanie predukaty:
+%	na/2
+% ===================================
+
+
+
+% na(X, Y)
+% Opis: spełniony, gdy klocek X leży
+% bezpośrednio na klocku Y
+% ===================================na/2
+	na(c, a).
+	na(c, b).
+	na(d, c).
+% ===================================na/2
+
+/*
+Informacje o budowie progamu:
+Program składa się z 3 klauzul.
+Program zawiera 1 definicję relacji.
+Jest to relacja na/2.
+Definicja realizacji na/2 składa się z
+3 klauzul, które są faktami.
+*/
